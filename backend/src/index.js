302e530ba6865eb1394f7097c8805a56ed9f54ee
Nithin <jvnithin2583@gmail.com>
2025-02-01 17:44:50 +0530

    second

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,42 +1,84 @@
-import express from "express"
-import dotenv from "dotenv"
+// import express from "express"
+// import dotenv from "dotenv"
+// import cookieParser from "cookie-parser";
+// import authRoutes from './routes/auth.route.js'
+// import messageRoutes from './routes/message.route.js'
+// import { connectDB } from "./lib/db.js"
+// import cors from "cors"
+// import {app,server} from "./lib/socket.js"
+// import path from "path"
+// dotenv.config()
+
+// const PORT=process.env.PORT
+
+// const __dirname=path.resolve();
+
+// app.use(express.json({ limit: '10mb' }));  // Increase the limit as needed
+
+// app.use(cookieParser());
+// app.use(cors(
+//     {
+//         origin:"http://localhost:5173",
+//         credentials:true
+//     }
+
+// ))
+
+// app.use("/api/auth",authRoutes)
+// app.use("/api/messages",messageRoutes)
+
+// if(process.env.NODE_ENV==="production"){
+//     app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+//   app.get("*", (req, res) => {
+//     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+//   });
+// }
+
+// server.listen(PORT,()=>{
+//     console.log("server is running at port : ",PORT)
+//     connectDB()
+// })
+
+import express from "express";
+import dotenv from "dotenv";
 import cookieParser from "cookie-parser";
-import authRoutes from './routes/auth.route.js'
-import messageRoutes from './routes/message.route.js'
-import { connectDB } from "./lib/db.js"
-import cors from "cors"
-import {app,server} from "./lib/socket.js"
-import path from "path"
-dotenv.config()
+import cors from "cors";
 
-const PORT=process.env.PORT
+import path from "path";
 
-const __dirname=path.resolve();
+import { connectDB } from "./lib/db.js";
 
-app.use(express.json({ limit: '10mb' }));  // Increase the limit as needed
+import authRoutes from "./routes/auth.route.js";
+import messageRoutes from "./routes/message.route.js";
+import { app, server } from "./lib/socket.js";
 
+dotenv.config();
+
+const PORT = process.env.PORT;
+const __dirname = path.resolve();
+
+app.use(express.json());
 app.use(cookieParser());
-app.use(cors(
-    {
-        origin:"http://localhost:5173",
-        credentials:true
-    }
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+    credentials: true,
+  })
+);
 
-))
+app.use("/api/auth", authRoutes);
+app.use("/api/messages", messageRoutes);
 
-app.use("/api/auth",authRoutes)
-app.use("/api/messages",messageRoutes)
-
-if(process.env.NODE_ENV==="production"){
-    app.use(express.static(path.join(__dirname, "../frontend/dist")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
   app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
   });
 }
 
-server.listen(PORT,()=>{
-    console.log("server is running at port : ",PORT)
-    connectDB()
-})
-
+server.listen(PORT, () => {
+  console.log("server is running on PORT:" + PORT);
+  connectDB();
+});
